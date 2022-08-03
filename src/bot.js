f4fc80560cfc470850a12e5c6ceafea09b2ec2bb
Tr1xN <tr1xn.gg@gmail.com>
2022-08-03 17:57:16 +0300

    + runner

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,5 @@
 import { Bot, session } from 'grammy';
+import { run } from "@grammyjs/runner";
 import moment from 'moment';
 import dotenv from 'dotenv';
 
@@ -31,14 +32,7 @@ creator.use(creatorComposer).use(adminComposer).use(defaultComposer);
 bot.catch((err) => {
     const ctx = err.ctx;
     console.error(`Error while handling update ${ctx.update.update_id}:`);
-    const e = err.error;
-    if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
-    } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
-    } else {
-        console.error("Unknown error:", e);
-    }
+    console.error(err);
 });
 
-bot.start()
+run(bot);
