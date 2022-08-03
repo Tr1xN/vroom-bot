f70cc600ef4abe5f76481e36d05b468bb45147d1
Tr1xN <tr1xn.gg@gmail.com>
2022-08-03 17:47:25 +0300

    error catch

--- src/bot.js
+++ src/bot.js
@@ -9,7 +9,6 @@ import { adminComposer } from './roles/admin.js'
 import { creatorComposer } from './roles/creator.js'
 import { defaultComposer } from './roles/default.js'
 
-
 dotenv.config()
 moment.locale('uk');
 connectToMongo();
@@ -28,5 +27,18 @@ guest.use(guestComposer);
 user.use(userComposer).use(defaultComposer);
 admin.use(adminComposer).use(defaultComposer);
 creator.use(creatorComposer).use(adminComposer).use(defaultComposer);
+
+bot.catch((err) => {
+    const ctx = err.ctx;
+    console.error(`Error while handling update ${ctx.update.update_id}:`);
+    const e = err.error;
+    if (e instanceof GrammyError) {
+        console.error("Error in request:", e.description);
+    } else if (e instanceof HttpError) {
+        console.error("Could not contact Telegram:", e);
+    } else {
+        console.error("Unknown error:", e);
+    }
+});
 
 bot.start()
